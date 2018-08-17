4cb3c4b752564c4abccee24a38a613346af29193
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-17 20:05:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function calculatesFarePrice (b1, b2) {
   if (distance <= 400) { 
     return 0; 
   } else if (distance > 400 && distance <= 2000) {
-    return distance*0.02;
+    return (distance-400)*0.02;
   } else if (distance > 2000 && distance <= 2500) {
     return 25;
   } else if (distance > 2500) {
