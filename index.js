08f7c3463dc75b683d5f405a1830a9888aeb8d8f
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-17 20:03:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,30 +14,13 @@ function distanceTravelledInFeet(b1, b2) {
 
 function calculatesFarePrice (b1, b2) { 
   const distance = distanceTravelledInFeet(b1, b2); 
-  
-  
   if (distance <= 400) { 
     return 0; 
-  } else if (distance > 400 && )
-  
-} 
-
-  describe('calculatesFarePrice(start, destination)', function() {
-    it('gives customers a free sample', function() {
-      expect(calculatesFarePrice(43, 44)).to.equal(0);
-    });
-
-    it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
-      // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
-      expect(calculatesFarePrice(34, 32)).to.equal(2.56);
-    });
-
-    it('charges 25 dollars for a distance over 2000 feet', function() {
-      expect(calculatesFarePrice(50, 58)).to.equal(25);
-    });
-
-    it('does not allow rides over 2500 feet', function() {
-      expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
-    });
-  });
-});
+  } else if (distance > 400 && distance <= 2000) {
+    return distance*0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else if (distance > 2500) {
+    return 'cannot travel that far';
+  }
+}
